cd5454e2797e42bc6e2a19586b1af8c33082084a
Santiago Pavon <santiago@dit.upm.es>
2018-02-02 15:35:19 +0100

    Paso 2.3.3 - Implementar el CLI.
    Sacar el código de los cases a funciones.

--- main.js
+++ main.js
@@ -20,57 +20,45 @@ rl
 
         case 'help':
         case 'h':
-            console.log("Commandos:");
-            console.log("  h|help - Muestra esta ayuda.");
-            console.log("  list - Listar los quizzes existentes.");
-            console.log("  show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
-            console.log("  add - Añadir un nuevo quiz interactivamente.");
-            console.log("  delete <id> - Borrar el quiz indicado.");
-            console.log("  edit <id> - Editar el quiz indicado.");
-            console.log("  test <id> - Probar el quiz indicado.");
-            console.log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
-            console.log("  credits - Créditos.");
-            console.log("  q|quit - Salir del programa.");
+            helpCmd();
             break;
 
         case 'quit':
         case 'q':
-            rl.close();
+            quitCmd();
             break;
 
         case 'add':
-            console.log('Añadir un nuevo quiz.');
+            addCmd();
             break;
 
         case 'list':
-            console.log('Listar todos los quizzes existentes.');
+            listCmd();
             break;
 
         case 'show':
-            console.log('Mostrar el quiz indicado.');
+            showCmd();
             break;
 
         case 'test':
-            console.log('Probar el quiz indicado.');
+            testCmd();
             break;
 
         case 'play':
         case 'p':
-            console.log('Jugar.');
+            playCmd();
             break;
 
         case 'delete':
-            console.log('Borrar el quiz indicado.');
+            deleteCmd();
             break;
 
         case 'edit':
-            console.log('Editar el quiz indicado.');
+            editCmd();
             break;
 
         case 'credits':
-            console.log('Autores de la práctica:');
-            console.log('Nombre 1');
-            console.log('Nombre 2');
+            creditsCmd();
             break;
 
         default:
@@ -84,3 +72,108 @@ rl
     console.log('Adios!');
     process.exit(0);
 });
+
+
+
+
+/**
+ * Muestra la ayuda.
+ */
+const helpCmd = () => {
+    console.log("Commandos:");
+    console.log("  h|help - Muestra esta ayuda.");
+    console.log("  list - Listar los quizzes existentes.");
+    console.log("  show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
+    console.log("  add - Añadir un nuevo quiz interactivamente.");
+    console.log("  delete <id> - Borrar el quiz indicado.");
+    console.log("  edit <id> - Editar el quiz indicado.");
+    console.log("  test <id> - Probar el quiz indicado.");
+    console.log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+    console.log("  credits - Créditos.");
+    console.log("  q|quit - Salir del programa.");
+};
+
+
+/**
+ * Lista todos los quizzes existentes en el modelo.
+ */
+const listCmd = () => {
+    console.log('Listar todos los quizzes existentes.');
+};
+
+
+/**
+ * Muestra el quiz indicado en el parámetro: la pregunta y la respuesta.
+ *
+ * @param id Clave del quiz a mostrar.
+ */
+const showCmd = id => {
+    console.log('Mostrar el quiz indicado.');
+};
+
+
+/**
+ * Añade un nuevo quiz al módelo.
+ * Pregunta interactivamente por la pregunta y por la respuesta.
+ */
+const addCmd = () => {
+    console.log('Añadir un nuevo quiz.');
+};
+
+
+
+/**
+ * Borra un quiz del modelo.
+ *
+ * @param id Clave del quiz a borrar en el modelo.
+ */
+const deleteCmd = id => {
+    console.log('Borrar el quiz indicado.');
+};
+
+
+/**
+ * Edita un quiz del modelo.
+ *
+ * @param id Clave del quiz a editar en el modelo.
+ */
+const editCmd = id => {
+    console.log('Editar el quiz indicado.');
+};
+
+
+/**
+ * Prueba un quiz, es decir, hace una pregunta del modelo a la que debemos contestar.
+ *
+ * @param id Clave del quiz a probar.
+ */
+const testCmd = id => {
+    console.log('Probar el quiz indicado.');
+};
+
+
+/**
+ * Pregunta todos los quizzes existentes en el modelo en orden aleatorio.
+ * Se gana si se contesta a todos satisfactoriamente.
+ */
+const playCmd = () => {
+    console.log('Jugar.');
+};
+
+
+/**
+ * Muestra los nombres de los autores de la práctica.
+ */
+const creditsCmd = () => {
+    console.log('Autores de la práctica:');
+    console.log('Nombre 1');
+    console.log('Nombre 2');
+};
+
+
+/**
+ * Terminar el programa.
+ */
+const quitCmd = () => {
+    rl.close();
+};
