d102c207a4eef9c63d78d9a1e9cdc6fcdc85a9ba
Santiago Pavon <santiago@dit.upm.es>
2018-02-02 15:15:04 +0100

    Paso 2.3.2 - Implementar el CLI.
    Completar el switch con entradas para todos los comandos.

--- main.js
+++ main.js
@@ -15,16 +15,72 @@ rl.prompt();
 rl
 .on('line', (line) => {
     switch (line.trim()) {
-        case 'hello':
-            console.log('world!');
+        case '':
             break;
+
+        case 'help':
+        case 'h':
+            console.log("Commandos:");
+            console.log("  h|help - Muestra esta ayuda.");
+            console.log("  list - Listar los quizzes existentes.");
+            console.log("  show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
+            console.log("  add - Añadir un nuevo quiz interactivamente.");
+            console.log("  delete <id> - Borrar el quiz indicado.");
+            console.log("  edit <id> - Editar el quiz indicado.");
+            console.log("  test <id> - Probar el quiz indicado.");
+            console.log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+            console.log("  credits - Créditos.");
+            console.log("  q|quit - Salir del programa.");
+            break;
+
+        case 'quit':
+        case 'q':
+            rl.close();
+            break;
+
+        case 'add':
+            console.log('Añadir un nuevo quiz.');
+            break;
+
+        case 'list':
+            console.log('Listar todos los quizzes existentes.');
+            break;
+
+        case 'show':
+            console.log('Mostrar el quiz indicado.');
+            break;
+
+        case 'test':
+            console.log('Probar el quiz indicado.');
+            break;
+
+        case 'play':
+        case 'p':
+            console.log('Jugar.');
+            break;
+
+        case 'delete':
+            console.log('Borrar el quiz indicado.');
+            break;
+
+        case 'edit':
+            console.log('Editar el quiz indicado.');
+            break;
+
+        case 'credits':
+            console.log('Autores de la práctica:');
+            console.log('Nombre 1');
+            console.log('Nombre 2');
+            break;
+
         default:
-            console.log(`Say what? I might have heard '${line.trim()}'`);
+            console.log(`Comando desconocido: '${line.trim()}'`);
+            console.log(`Use 'help' para ver todos los comandos disponibles.`);
             break;
     }
     rl.prompt();
 })
 .on('close', () => {
-    console.log('Have a great day!');
+    console.log('Adios!');
     process.exit(0);
 });
