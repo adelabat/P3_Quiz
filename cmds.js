dc253deddd8f882c098918008d04fc723f94770f
Santiago Pavon <santiago@dit.upm.es>
2018-02-02 17:01:26 +0100

    Paso 2.7.3 - Crear el módulo cmds.

---
+++ cmds.js
@@ -0,0 +1,134 @@
+
+
+const {log, biglog, errorlog, colorize} = require("./out");
+
+const model = require('./model');
+
+
+/**
+ * Muestra la ayuda.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.helpCmd = rl => {
+    log("Commandos:");
+    log("  h|help - Muestra esta ayuda.");
+    log("  list - Listar los quizzes existentes.");
+    log("  show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
+    log("  add - Añadir un nuevo quiz interactivamente.");
+    log("  delete <id> - Borrar el quiz indicado.");
+    log("  edit <id> - Editar el quiz indicado.");
+    log("  test <id> - Probar el quiz indicado.");
+    log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
+    log("  credits - Créditos.");
+    log("  q|quit - Salir del programa.");
+    rl.prompt();
+};
+
+
+/**
+ * Lista todos los quizzes existentes en el modelo.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.listCmd = rl => {
+    log('Listar todos los quizzes existentes.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Muestra el quiz indicado en el parámetro: la pregunta y la respuesta.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a mostrar.
+ */
+exports.showCmd = (rl, id) => {
+    log('Mostrar el quiz indicado.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Añade un nuevo quiz al módelo.
+ * Pregunta interactivamente por la pregunta y por la respuesta.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.addCmd = rl => {
+    log('Añadir un nuevo quiz.', 'red');
+    rl.prompt();
+};
+
+
+
+/**
+ * Borra un quiz del modelo.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a borrar en el modelo.
+ */
+exports.deleteCmd = (rl, id) => {
+    log('Borrar el quiz indicado.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Edita un quiz del modelo.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a editar en el modelo.
+ */
+exports.editCmd = (rl, id) => {
+    log('Editar el quiz indicado.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Prueba un quiz, es decir, hace una pregunta del modelo a la que debemos contestar.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ * @param id Clave del quiz a probar.
+ */
+exports.testCmd = (rl, id) => {
+    log('Probar el quiz indicado.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Pregunta todos los quizzes existentes en el modelo en orden aleatorio.
+ * Se gana si se contesta a todos satisfactoriamente.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.playCmd = rl => {
+    log('Jugar.', 'red');
+    rl.prompt();
+};
+
+
+/**
+ * Muestra los nombres de los autores de la práctica.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.creditsCmd = rl => {
+    log('Autores de la práctica:');
+    log('Nombre 1', 'green');
+    log('Nombre 2', 'green');
+    rl.prompt();
+};
+
+
+/**
+ * Terminar el programa.
+ *
+ * @param rl Objeto readline usado para implementar el CLI.
+ */
+exports.quitCmd = rl => {
+    rl.close();
+};
+
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const readline = require('readline');
 
 const {log, biglog, errorlog, colorize} = require("./out");
 
-const model = require('./model');
+const cmds = require("./cmds");
 
 
 // Mensaje inicial
@@ -36,45 +36,45 @@ rl
 
         case 'help':
         case 'h':
-            helpCmd();
+            cmds.helpCmd(rl);
             break;
 
         case 'quit':
         case 'q':
-            quitCmd();
+            cmds.quitCmd(rl);
             break;
 
         case 'add':
-            addCmd();
+            cmds.addCmd(rl);
             break;
 
         case 'list':
-            listCmd();
+            cmds.listCmd(rl);
             break;
 
         case 'show':
-            showCmd(args[1]);
+            cmds.showCmd(rl, args[1]);
             break;
 
         case 'test':
-            testCmd(args[1]);
+            cmds.testCmd(rl, args[1]);
             break;
 
         case 'play':
         case 'p':
-            playCmd();
+            cmds.playCmd(rl);
             break;
 
         case 'delete':
-            deleteCmd(args[1]);
+            cmds.deleteCmd(rl, args[1]);
             break;
 
         case 'edit':
-            editCmd(args[1]);
+            cmds.editCmd(rl, args[1]);
             break;
 
         case 'credits':
-            creditsCmd();
+            cmds.creditsCmd(rl);
             break;
 
         default:
@@ -90,115 +90,3 @@ rl
 });
 
 
-
-
-/**
- * Muestra la ayuda.
- */
-const helpCmd = () => {
-    log("Commandos:");
-    log("  h|help - Muestra esta ayuda.");
-    log("  list - Listar los quizzes existentes.");
-    log("  show <id> - Muestra la pregunta y la respuesta el quiz indicado.");
-    log("  add - Añadir un nuevo quiz interactivamente.");
-    log("  delete <id> - Borrar el quiz indicado.");
-    log("  edit <id> - Editar el quiz indicado.");
-    log("  test <id> - Probar el quiz indicado.");
-    log("  p|play - Jugar a preguntar aleatoriamente todos los quizzes.");
-    log("  credits - Créditos.");
-    log("  q|quit - Salir del programa.");
-    rl.prompt();
-};
-
-
-/**
- * Lista todos los quizzes existentes en el modelo.
- */
-const listCmd = () => {
-    log('Listar todos los quizzes existentes.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Muestra el quiz indicado en el parámetro: la pregunta y la respuesta.
- *
- * @param id Clave del quiz a mostrar.
- */
-const showCmd = id => {
-    log('Mostrar el quiz indicado.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Añade un nuevo quiz al módelo.
- * Pregunta interactivamente por la pregunta y por la respuesta.
- */
-const addCmd = () => {
-    log('Añadir un nuevo quiz.', 'red');
-    rl.prompt();
-};
-
-
-
-/**
- * Borra un quiz del modelo.
- *
- * @param id Clave del quiz a borrar en el modelo.
- */
-const deleteCmd = id => {
-    log('Borrar el quiz indicado.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Edita un quiz del modelo.
- *
- * @param id Clave del quiz a editar en el modelo.
- */
-const editCmd = id => {
-    log('Editar el quiz indicado.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Prueba un quiz, es decir, hace una pregunta del modelo a la que debemos contestar.
- *
- * @param id Clave del quiz a probar.
- */
-const testCmd = id => {
-    log('Probar el quiz indicado.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Pregunta todos los quizzes existentes en el modelo en orden aleatorio.
- * Se gana si se contesta a todos satisfactoriamente.
- */
-const playCmd = () => {
-    log('Jugar.', 'red');
-    rl.prompt();
-};
-
-
-/**
- * Muestra los nombres de los autores de la práctica.
- */
-const creditsCmd = () => {
-    log('Autores de la práctica:');
-    log('Nombre 1', 'green');
-    log('Nombre 2', 'green');
-    rl.prompt();
-};
-
-
-/**
- * Terminar el programa.
- */
-const quitCmd = () => {
-    rl.close();
-};
